ba15c7dba8dff5a5d2770e43a5f63d78386591d2
whdlgp <whdlgp@gmail.com>
2020-04-11 01:30:49 +0900

    Add Mouse point magnification window for manual test

--- manual_point_pickup_test/main.cpp
+++ manual_point_pickup_test/main.cpp
@@ -11,9 +11,13 @@ using namespace std;
 using namespace cv;
 
 #define OUTPUT_HEIGHT 960
+#define CROP_WINDOW_SIZE 11
+#define MOUSE_OFFSET_MAX 3
+#define BORDER ((CROP_WINDOW_SIZE/2) + MOUSE_OFFSET_MAX)
 #define WINDOW_NAME "test_show"
+#define MOUSE_WINDOW_NAME "mouse_point_magnification"
 
-static void imshow_resize(Mat& im_left, Mat& im_right)
+static void imshow_resize(Mat& im_left, Mat& im_right, Point2i mouse_pt)
 {
     double resize_ratio = double(OUTPUT_HEIGHT)/double(im_left.rows*2);
     Mat im_left_resize, im_right_resize;
@@ -23,6 +27,36 @@ static void imshow_resize(Mat& im_left, Mat& im_right)
     Mat show_mat;
     vconcat(show_vector, show_mat);
     imshow(WINDOW_NAME, show_mat);
+
+    Mat mouse_window;
+    Mat im_left_border, im_right_border;
+    copyMakeBorder(im_left, im_left_border
+                   , BORDER, BORDER, BORDER, BORDER
+                   , BORDER_CONSTANT);
+    copyMakeBorder(im_right, im_right_border
+                   , BORDER, BORDER, BORDER, BORDER
+                   , BORDER_CONSTANT);
+    if(mouse_pt.y < im_left.rows)
+    {
+        int x = BORDER + mouse_pt.x;
+        int y = BORDER + mouse_pt.y;
+        mouse_window = im_left_border(Rect(x-(CROP_WINDOW_SIZE/2)
+                                            , y-(CROP_WINDOW_SIZE/2)
+                                            , CROP_WINDOW_SIZE
+                                            , CROP_WINDOW_SIZE));
+    }
+    else
+    {
+        int x = BORDER + mouse_pt.x;
+        int y = BORDER + mouse_pt.y - im_left.rows;
+        mouse_window = im_right_border(Rect(x-(CROP_WINDOW_SIZE/2)
+                                            , y-(CROP_WINDOW_SIZE/2)
+                                            , CROP_WINDOW_SIZE
+                                            , CROP_WINDOW_SIZE));
+    }
+    mouse_window.at<Vec3b>((CROP_WINDOW_SIZE/2), (CROP_WINDOW_SIZE/2)) = Vec3b(0, 255, 0);
+    resize(mouse_window, mouse_window, Size(200, 200), 0, 0, CV_INTER_NN);
+    imshow(MOUSE_WINDOW_NAME, mouse_window);
 }
 
 enum
@@ -33,10 +67,24 @@ enum
 static int point_state = LEFT_POINT;
 static vector<Point2d> click_points_left;
 static vector<Point2d> click_points_right;
+typedef struct
+{
+    Point2i pt;
+    double resize_ratio;
+    int x_offset;
+    int y_offset;
+} callback_param_t;
 static void pick_point(int evt, int x, int y, int flags, void* param)
 {
-    double resize_ratio = *((double*)param);
-    if(evt == CV_EVENT_LBUTTONDOWN)
+    callback_param_t* callback_param = (callback_param_t*)param;
+    double resize_ratio = callback_param->resize_ratio;
+    if(evt == CV_EVENT_MOUSEMOVE)
+    {
+        callback_param->pt = Point2i(x, y)/resize_ratio;
+        callback_param->pt.x += callback_param->x_offset;
+        callback_param->pt.y += callback_param->y_offset;
+    }
+    else if(evt == CV_EVENT_LBUTTONDOWN)
     {
         Rect window_rect = getWindowImageRect(WINDOW_NAME);
         if(point_state == LEFT_POINT)
@@ -44,7 +92,7 @@ static void pick_point(int evt, int x, int y, int flags, void* param)
             if(y < window_rect.height/2)
             {
                 cout << "left point input" << endl;
-                Point2i input = Point(x, y)*(1/resize_ratio);
+                Point2i input = callback_param->pt;
                 click_points_left.push_back(input);
                 cout << "Point x: " << input.x << " Point y: " << input.y << endl;
                 cout << "num of left point: " << click_points_left.size() << endl;
@@ -61,7 +109,7 @@ static void pick_point(int evt, int x, int y, int flags, void* param)
             else
             {
                 cout << "right point input" << endl;
-                Point2i input = Point(x, y - window_rect.height/2)*(1/resize_ratio);
+                Point2i input = Point2i(callback_param->pt.x, callback_param->pt.y - (window_rect.height/2)/resize_ratio);
                 click_points_right.push_back(input);
                 cout << "Point x: " << input.x << " Point y: " << input.y << endl;
                 cout << "num of left point: " << click_points_left.size() << endl;
@@ -157,9 +205,23 @@ int main(int argc, char* argv[])
     Mat im_left = imread(im_left_name);
     Mat im_right = imread(im_right_name);
 
+    callback_param_t callback_param;
+    callback_param.resize_ratio = double(OUTPUT_HEIGHT)/double(im_left.rows*2);
     double resize_ratio = double(OUTPUT_HEIGHT)/double(im_left.rows*2);
+    
     namedWindow(WINDOW_NAME);
-    setMouseCallback(WINDOW_NAME, pick_point, (void*)&resize_ratio);
+    setMouseCallback(WINDOW_NAME, pick_point, (void*)&callback_param);
+
+    namedWindow(MOUSE_WINDOW_NAME);
+    createTrackbar("x_offset", MOUSE_WINDOW_NAME, &(callback_param.x_offset), 7);
+    createTrackbar("y_offset", MOUSE_WINDOW_NAME, &(callback_param.y_offset), 7);
+    setTrackbarMin("x_offset", MOUSE_WINDOW_NAME, -3);
+    setTrackbarMax("x_offset", MOUSE_WINDOW_NAME, 3);
+    setTrackbarMin("y_offset", MOUSE_WINDOW_NAME, -3);
+    setTrackbarMax("y_offset", MOUSE_WINDOW_NAME, 3);
+    setTrackbarPos("x_offset", MOUSE_WINDOW_NAME, 0);
+    setTrackbarPos("y_offset", MOUSE_WINDOW_NAME, 0);
+
     while(1)
     {
         Mat im_left_tmp = im_left.clone();
@@ -174,7 +236,7 @@ int main(int argc, char* argv[])
             for(int i = 0; i < click_points_left.size(); i++)
                 circle(im_right_tmp, click_points_right[i], 5, Scalar(255, 0, 0), 3);
         }
-        imshow_resize(im_left_tmp, im_right_tmp);
+        imshow_resize(im_left_tmp, im_right_tmp, callback_param.pt);
         waitKey(10);
     }
 
